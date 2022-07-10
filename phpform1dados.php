51d9b9984983846722178a07261f67f0a8b6c51b
Ryan Costa <89921782+ryancosta15@users.noreply.github.com>
2022-07-10 01:22:36 -0300

    3.0
    Consideradas sugestões de  Rafael Cunha

--- phpform1dados.php
+++ phpform1dados.php
@@ -13,43 +13,45 @@
     $age = $_POST['age'];
 
     //message depending on the age
-    if ($age >= 0.1 && $age <= 1) {
-      $agemsg = "Bebê";
-      $ageimg = "babyimg.jpg";
-      $agealt = "bebê";
-    }   elseif ($age >= 2 && $age < 12 ) {
+    switch ($age){
+        case ($age >= 0.1 && $age <= 1):
+            $agemsg = "Bebê";
+            $ageimg = "babyimg.jpg";
+            $agealt = "bebê";
+        break;
+        case ($age >= 2 && $age < 12):
             $agemsg = "Criança";
             $ageimg = "kidimg.jpg";
             $agealt = "criança";
-    }   elseif ($age >= 12 && $age < 18 ) {
+            break;
+        case ($age >= 12 && $age < 18): 
             $agemsg = "Adolescente";
             $ageimg = "adolescente.jpg";
             $agealt = "adolescente";
-    }   elseif ($age >= 18 && $age < 59 ) {
+            break;
+        case ($age >= 18 && $age < 59):
             $agemsg = "Adulto";
             $ageimg = "adulto.jpg";
             $agealt = "adulto";
-    } elseif ($age >=60 && $age <=119) {
-        $agemsg = "Idoso";
-        $ageimg = "idosoimg.jpg";
-        $agealt = "idoso";
-    } elseif ($age >119 ) {
-        $agemsg = "morto";
-        $ageimg = "morto.jpg";
-        $agealt = "cova";
-    }   else {
-        $agemsg = "feto";
-        $ageimg = "feto.jpg";
-        $agealt = "feto";
+            break;
+        case ($age >=60 && $age <=119): 
+            $agemsg = "Idoso";
+            $ageimg = "idosoimg.jpg";
+            $agealt = "idoso";
+            break;
+        case ($age >119 ): 
+            $agemsg = "morto";
+            $ageimg = "morto.jpg";
+            $agealt = "cova";
+            break;
+        default: 
+            $agemsg = "feto";
+            $ageimg = "feto.jpg";
+            $agealt = "feto";      
     }
     ?>
-    <h1>
-        <?php
-        echo "Seja bem vindo $name"
-        ?>
-    </h1>
     <h2>Você é um</h2>
-    <h1 class="redtxt">
+    <h1 class="agemsg">
         <?php echo "$agemsg" ?>
     </h1>
     <figure>
@@ -65,36 +67,43 @@
     $imc = $weight / $height **2;
     
     //message depending on the imc
-    if ($imc >=1 &&$imc < 18.5 ) {
-        $imcmsg = "Abaixo do peso";
-        $imcimg = "abaixodopeso.jpg";
-        $imcalt = "esqueleto";
-    } elseif ($imc >= 18.5 && $imc < 24.9) {
+    switch ($imc){
+        case ($imc >= 1 &&$imc < 18.5):
+            $imcmsg = "Abaixo do peso";
+            $imcimg = "abaixodopeso.jpg";
+            $imcalt = "esqueleto";
+            break;
+        case ($imc >= 18.5 && $imc < 24.9):
             $imcmsg = "Peso ideal";
             $imcimg = "pesoideal.jpg";
             $imcalt = "doutora feliz";
-    } elseif ($imc >= 25 && $imc <= 29.9) {
+            break;
+        case ($imc >= 25 && $imc <= 29.9):
             $imcmsg = "Sobrepeso";
             $imcimg = "sobrepeso.jpg";
             $imcalt = "barriga de chopp";
-    } elseif ($imc >= 30 && $imc <= 34.9) {
+            break;
+        case ($imc >= 30 && $imc <= 34.9):
             $imcmsg = "Obesidade grau I";
             $imcimg = "obesidade1.jpg";
             $imcalt = "gordox";
-    } elseif ($imc >= 35 && $imc <= 39.9) {
+            break;
+        case ($imc >= 35 && $imc <= 39.9):
             $imcmsg = "Obesidade grau II";
             $imcimg = "obesidade2.jpg";
             $imcalt = "pessoa rindo da comida";
-    } elseif ($imc >= 40) {
-        $imcmsg = "Obesidade grau III (vai pro médico pf)";
-        $imcimg = "obesidade3.jpg";
-        $imcalt = "pessoa no médico";
-    } else {
-        $imcmsg = "Você não existe";
-        $imcimg = "fantasma.jpg";
-        $imcalt = "fantasma";
+            break;
+        case ($imc >= 40):
+            $imcmsg = "Obesidade grau III (vai pro médico pf)";
+            $imcimg = "obesidade3.jpg";
+            $imcalt = "pessoa no médico";
+            break;
+        default:
+            $imcmsg = "Você não existe";
+            $imcimg = "fantasma.jpg";
+            $imcalt = "fantasma";
     }   
-
+    
     //show just 4 characters
     if(strlen($imc) > 4){
      $nimc = substr($imc, 0, 4);
@@ -103,7 +112,7 @@
     }
     ?>
     <h2>Seu IMC é</h2>
-    <h1 class="redtxt">
+    <h1 class="agemsg">
         <?php echo "$nimc ($imcmsg)" ?>
     </h1>
     <figure>
